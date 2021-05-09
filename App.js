9e6de014865c0d59b637fec3601e9ba5207165d0
Вячеслав Филиппов <filislav43@gmail.com>
2021-05-09 09:09:17 +0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, SafeAreaView, Button,Alert,Image,TouchableHighlight,TouchableWithoutFeedback} from 'react-native';
+import styles from './styles';
 
 export default function App() {
+  let result = 1;
+  let onCklickText = ()=>{
+      console.log(' ok text pressed');
+  };
+  const handleOkButton = ()=>{
+      console.log("Ok");
+  };
+  const handleCnacelButton = ()=>{
+    console.log("Cancel");
+  };
+  const handleButtonPress = ()=>{
+      Alert.alert("Result:",'Button Pressed',[{text:'Ok',onPress:handleOkButton}
+      ,{text:"Cancel",onPress:handleCnacelButton}]);
+  };
+  const handleAnothButt = (text)=>{
+    console.log(Number(text)+Number(result));
+  }
+  const handleAnothPress = ()=>{
+    Alert.prompt("Another result","Enter value",handleAnothButt);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text} onPress={onCklickText}>Hello Slava!!! {'\n'}I'm looking for you </Text>
+      <Button color="red" title="Click Me" onPress={handleButtonPress}/>
+      <Button color="green" title="Another button" onPress={handleAnothPress}/>
+      <Image source={require("./assets/favicon.png")}/>
+      {/* <TouchableHighlight onPress={onCklickText}>
+      <Image source={
+        {
+          width:200,
+          height:150,
+          uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Desktop_computer_clipart_-_Yellow_theme.svg/1200px-Desktop_computer_clipart_-_Yellow_theme.svg.png"
+        }
+      }/>
+      </TouchableHighlight> */}
+      <TouchableWithoutFeedback onPress={onCklickText}>
+      <Image source={
+        {
+          width:200,
+          height:150,
+          uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Desktop_computer_clipart_-_Yellow_theme.svg/1200px-Desktop_computer_clipart_-_Yellow_theme.svg.png"
+        }
+      }/>
+      </TouchableWithoutFeedback> 
+      <StatusBar style="auto" />      
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'yellow',
+    },
+    text: {
+      color:'red',
+      borderStyle:'solid',
+      borderWidth:1,
+      backgroundColor:'white',
+      borderRadius:3
+    },
+    button: {
+      color:'red',
+      borderStyle:'solid',
+      borderWidth:1,
+      borderColor:'green'
+    }
+  });
+
+  export default styles;
